5efc459042ee10c039acdf0c2c8f65bf3cd17a46
Artem Mezentsev <artemamway88@gmail.com>
2021-02-01 22:07:34 +0300

    Добавил переход по видео по клику на дорожу, отобразил время, удобное для понимания. Добавил делегирование: клик по экрану также вкл/выкл видео

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-let playerScreen = document.querySelector('.player__screen');
-let panelSwitch = document.querySelector('.panel__switch');
-let panelTime = document.querySelector('.panel__time');
+let screen = document.querySelector('.player__screen');
+let button = document.querySelector('.panel__switch');
+let time = document.querySelector('.panel__time');
 let panelSlider = document.querySelector('.panel__slider');
 
-playerScreen.ontimeupdate = () => {
-  panelSlider.value = 100 * playerScreen.currentTime / playerScreen.duration;
-  panelTime.innerHTML = playerScreen.currentTime.toFixed(2);
-  if(playerScreen.ended) {
-    panelSwitch.classList.toggle('active');
+function currentTime() {
+  let seconds = Math.floor(screen.currentTime % 60),
+    minutes = Math.floor(screen.currentTime / 60),
+    hours = Math.floor(screen.currentTime / 60);
+
+  hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+   return (seconds > 3599)
+    ? `${hours}:${minutes}:${seconds}`
+    : `${minutes}:${seconds}`;
+}
+
+screen.ontimeupdate = () => {
+  panelSlider.value = 100 * screen.currentTime / screen.duration;
+  time.innerHTML = currentTime();
+  if(screen.ended) {
+    button.classList.toggle('active');
   }
 };
 
-panelSwitch.addEventListener('click', () => {
-  panelSwitch.classList.toggle('active');
-  if(playerScreen.paused) { 
-    playerScreen.play();
+document.querySelector('.section').addEventListener('click', event => {
+  let target = event.target.className;
+  if((target === 'player__screen') || (target === 'panel__switch') || (target === 'panel__switch active')) {
+    button.classList.toggle('active');
+    if(screen.paused) { 
+      screen.play();
+    }
+    else {
+      screen.pause();
+    }
   }
-  else {
-    playerScreen.pause();
-  }
+});
+
+panelSlider.addEventListener('input', () => {
+  screen.currentTime = screen.duration * panelSlider.value / 100;
 });
