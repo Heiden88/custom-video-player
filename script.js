ce3d877f6a36a69d2844348e6f18e862cfd36ea7
Artem Mezentsev <artemamway88@gmail.com>
2021-02-02 20:20:09 +0300

    Добавил микшер громкости, svg картинку, изменил видео

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let screen = document.querySelector('.player__screen');
 let button = document.querySelector('.panel__switch');
 let time = document.querySelector('.panel__time');
 let panelSlider = document.querySelector('.panel__slider');
+let mixer = document.querySelector('.mixer__volume');
 
 function currentTime() {
   let seconds = Math.floor(screen.currentTime % 60),
@@ -12,13 +13,20 @@ function currentTime() {
   minutes = (minutes < 10) ? "0" + minutes : minutes;
   seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-   return (seconds > 3599)
+   return (screen.duration > 3599)
     ? `${hours}:${minutes}:${seconds}`
     : `${minutes}:${seconds}`;
 }
 
 screen.ontimeupdate = () => {
   panelSlider.value = 100 * screen.currentTime / screen.duration;
+  screen.volume = mixer.value / 10;
+  if(screen.volume === 0) {
+    document.querySelector('.mixer').classList.add('mixer_active');
+  }
+  else {
+    document.querySelector('.mixer').classList.remove('mixer_active');
+  }
   time.innerHTML = currentTime();
   if(screen.ended) {
     button.classList.toggle('active');
